fe2c3f2f139f8261b97186a9917ee0f769e30faa
Christopher Miller <admin@mctherealm.net>
2017-02-14 22:00:43 -0500

    Added main css file and menu options

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const {electron, BrowserWindow, app, Menu} = require('electron')
 
 const path = require('path')
 const url = require('url')
@@ -21,9 +17,6 @@ function createWindow () {
     protocol: 'file:',
     slashes: true
   }))
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -58,3 +51,63 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New'
+      },
+      {
+        label: 'Open'
+      },
+      {
+        label: 'Save'
+      },
+      {
+        label: 'Save As'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        role: 'undo'
+      },
+      {
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'cut'
+      },
+      {
+        role: 'copy'
+      },
+      {
+        role: 'paste'
+      },
+      {
+        role: 'pasteandmatchstyle'
+      },
+      {
+        role: 'delete'
+      },
+      {
+        role: 'selectall'
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
